f34c9b809782d3e0dc13ae7538f49acc163ae9e8
Brandon Ramirez <brandon.ramirez@live.com>
2018-06-29 14:40:50 -0700

    Improve data source, add ParamChangeRegistry data set

---
+++ mappings/meme.ts
@@ -0,0 +1,147 @@
+/** Contract Helpers */
+class Helpers {
+  static registryEntry(regEntry: Meme__loadRegistryEntryResult): Entity {
+    let entity = new Entity()
+    entity.setU32('regEntry_version', Token.fromU256(regEntry.value0).toU32())
+    entity.setU32('regEntry_status', regEntry.value1)
+    entity.setString('regEntry_creator', regEntry.value2.toHex())
+    entity.setU32('regEntry_deposit', Token.fromU256(regEntry.value3).toU32())
+    entity.setU256('regEntry_challengePeriodEnd', regEntry.value4)
+    return entity
+  }
+
+  static registryEntryChallenge(
+    regEntryChallenge: Meme__loadRegistryEntryChallengeResult
+  ): Entity {
+    let votesFor = Token.fromU256(regEntryChallenge.value6).toU32()
+    let votesAgainst = Token.fromU256(regEntryChallenge.value7).toU32()
+    let rewardPool = Token.fromU256(regEntryChallenge.value2).toU32()
+
+    let entity = new Entity()
+    entity.setString('challenge_challenger', regEntryChallenge.value1.toHex())
+    entity.setU32('challenge_rewardPool', rewardPool)
+    entity.setBytes('challenge_metaHash', regEntryChallenge.value3)
+    entity.setU256('challenge_commitPeriodEnd', regEntryChallenge.value4)
+    entity.setU256('challenge_revealPeriodEnd', regEntryChallenge.value5)
+    entity.setU32('challenge_votesFor', votesFor)
+    entity.setU32('challenge_votesAgainst', votesAgainst)
+    entity.setU32('challenge_votesTotal', votesFor + votesAgainst)
+    entity.setU256('challenge_claimedRewardOn', regEntryChallenge.value8)
+    return entity
+  }
+
+  static meme(meme: Meme__loadMemeResult): Entity {
+    let entity = new Entity()
+    entity.setBytes('meme_metaHash', meme.value0)
+    entity.setU32('meme_totalSupply', Token.fromU256(meme.value1).toU32())
+    entity.setU32('meme_totalMinted', Token.fromU256(meme.value2).toU32())
+    entity.setU32('meme_tokenIdStart', Token.fromU256(meme.value3).toU32())
+    return entity
+  }
+}
+
+export function handleMemeRegistryEntryEvent(event: RegistryEntryEvent): void {
+  // Extract event arguments
+  let registryEntryAddress = event.registryEntry
+  let eventType = event.eventType.toString()
+
+  // Create an instance of the 'Meme' contract
+  let memeContract = Meme.bind(registryEntryAddress, event.blockHash)
+
+  // Obtain registry entry and meme data from the contract
+  let memeData = memeContract.loadMeme()
+  let entryData = memeContract.loadRegistryEntry()
+  let challengeData = memeContract.loadRegistryEntryChallenge()
+
+  if (eventType == 'constructed') {
+    // Create the meme creator
+    let creatorAddress = entryData.value2
+    let creator = new Entity()
+    creator.setString('id', creatorAddress.toHex())
+    creator.setString('user_address', creatorAddress.toHex())
+    database.create('User', creatorAddress.toHex(), creator)
+
+    // Create the new meme
+    let meme = Helpers.registryEntry(entryData).merge([
+      Helpers.registryEntryChallenge(challengeData),
+      Helpers.meme(memeData),
+    ])
+    meme.setString('id', registryEntryAddress.toHex())
+    meme.setAddress('regEntry_address', registryEntryAddress)
+    meme.setU256('regEntry_createdOn', event.timestamp)
+    database.create('Meme', registryEntryAddress.toHex(), meme)
+  } else if (eventType == 'challengeCreated') {
+    // Create challenger
+    let challenger = new Entity()
+    let challengerAddress = challengeData.value1
+    challenger.setString('id', challengerAddress.toHex())
+    challenger.setAddress('user_address', challengerAddress)
+    database.create('User', challengerAddress.toHex(), challenger)
+
+    // Update meme
+    let meme = Helpers.registryEntry(entryData).merge([
+      Helpers.registryEntryChallenge(challengeData),
+      Helpers.meme(memeData),
+    ])
+    meme.setAddress('regEntry_address', registryEntryAddress)
+    meme.setU256('challenge_createdOn', event.timestamp)
+    database.update('Meme', registryEntryAddress.toHex(), meme)
+  } else if (eventType == 'voteCommitted') {
+    // Obtain voter address
+    let eventData = event.data
+    let voterAddress = eventData[0].toAddress()
+
+    // Create the voter
+    let voter = new Entity()
+    voter.setString('id', voterAddress.toHex())
+    voter.setAddress('user_address', voterAddress)
+    database.create('User', voterAddress.toHex(), voter)
+
+    // Create the vote
+    let voteData = memeContract.loadVote(voterAddress)
+    let voteId = registryEntryAddress.toHex() + '-' + voterAddress.toHex()
+    let vote = new Entity()
+    vote.setString('id', voteId)
+    vote.setString('vote_voter', voterAddress.toHex())
+    vote.setAddress('regEntry_address', registryEntryAddress)
+    vote.setString('vote_secretHash', voteData.value0.toHex())
+    vote.setU32('vote_option', voteData.value1)
+    vote.setU32('vote_amount', Token.fromU256(voteData.value2).toU32())
+    vote.setU256('vote_revealedOn', voteData.value3)
+    vote.setU256('vote_claimedRewardOn', voteData.value4)
+    vote.setU256('vote_createdOn', event.timestamp)
+    database.create('Vote', voteId, vote)
+  } else if (eventType == 'voteRevealed') {
+    // Obtain the voter address
+    let eventData = event.data
+    let voterAddress = eventData[0].toAddress()
+
+    // Update the vote
+    let voteData = memeContract.loadVote(voterAddress)
+    let voteId = registryEntryAddress.toHex() + '-' + voterAddress.toHex()
+    let vote = new Entity()
+    vote.setAddress('vote_voter', voterAddress)
+    vote.setU32('vote_option', voteData.value1)
+    vote.setU256('vote_revealedOn', voteData.value3)
+    database.update('Vote', voteId, vote)
+
+    // Update the meme
+    let meme = Helpers.registryEntryChallenge(challengeData)
+    database.update('Meme', registryEntryAddress.toHex(), meme)
+  } else if (eventType == 'challengeRewardClaimed') {
+    // Update the meme
+    let meme = Helpers.registryEntryChallenge(challengeData)
+    database.update('Meme', registryEntryAddress.toHex(), meme)
+  } else if (eventType == 'depositTransferred') {
+    // Update the meme
+    let meme = Helpers.registryEntry(entryData)
+    database.update('Meme', registryEntryAddress.toHex(), meme)
+  } else if (eventType == 'minted') {
+    // Update the meme
+    let meme = Helpers.registryEntry(entryData).merge([
+      Helpers.registryEntryChallenge(challengeData),
+      Helpers.meme(memeData),
+    ])
+    database.update('Meme', registryEntryAddress.toHex(), meme)
+  }
+}
---
+++ mappings/param-change.ts
@@ -0,0 +1,79 @@
+/** Contract Helpers */
+class Helpers {
+  static registryEntry(regEntry: ParamChange__loadRegistryEntryResult): Entity {
+    let entity = new Entity()
+    entity.setU256('regEntry_version', regEntry.value0)
+    entity.setU32('regEntry_status', regEntry.value1)
+    entity.setAddress('regEntry_creator', regEntry.value2)
+    entity.setU256('regEntry_deposit', regEntry.value3)
+    entity.setU256('regEntry_challengePeriodEnd', regEntry.value4)
+    return entity
+  }
+
+  static registryEntryChallenge(
+    regEntryChallenge: ParamChange__loadRegistryEntryChallengeResult
+  ): Entity {
+    let entity = new Entity()
+    entity.setAddress('challenge_challenger', regEntryChallenge.value1)
+    entity.setU256('challenge_rewardPool', regEntryChallenge.value2)
+    entity.setBytes('challenge_metaHash', regEntryChallenge.value3)
+    entity.setU256('challenge_commitPeriodEnd', regEntryChallenge.value4)
+    entity.setU256('challenge_revealPeriodEnd', regEntryChallenge.value5)
+    entity.setU256('challenge_votesFor', regEntryChallenge.value6)
+    entity.setU256('challenge_votesAgainst', regEntryChallenge.value7)
+    entity.setU256('challenge_claimedRewardOn', regEntryChallenge.value8)
+    return entity
+  }
+
+  static paramChange(paramChange: ParamChange__loadParamChangeResult): Entity {
+    let entity = new Entity()
+
+    entity.setAddress('paramChange_db', paramChange.value0)
+    entity.setString('paramChange_key', paramChange.value1)
+    entity.setU32('paramChange_valueType', paramChange.value2)
+    entity.setU256('paramChange_value', paramChange.value3)
+    entity.setU256('paramChange_appliedOn', paramChange.value4)
+
+    return entity
+  }
+}
+
+export function handleParamRegistryEntryEvent(event: RegistryEntryEvent): void {
+  // Extract event arguments
+  let registryEntryAddress = event.registryEntry
+  let eventType = event.eventType.toString()
+
+  if (eventType == 'constructed') {
+    //let paramChangeContract = ParamChange.bind(registryEntryAddress, event.blockHash)
+    //let paramChangeData = paramChangeContract.loadParamChange()
+    //let registryEntryData = paramChangeContract.loadRegistryEntry()
+    //let registryEntryChallengeData = paramChangeContract.loadRegistryEntryChallenge()
+    //let paramChange = Helpers.registryEntry(registryEntryData).merge([
+    //  Helpers.registryEntryChallenge(registryEntryChallengeData),
+    //  Helpers.paramChange(paramChangeData),
+    //])
+    //paramChange.setString('id', registryEntryAddress.toString())
+    //paramChange.setAddress('regEntry_address', registryEntryAddress)
+    //paramChange.setU256('regEntry_createdOn', event.timestamp)
+    //database.create('ParamChange', registryEntryAddress.toString(), paramChange)
+  } else if (eventType == 'challengeCreated') {
+    //let paramChangeContract = ParamChange.bind(registryEntryAddress, event.blockHash)
+    //let registryEntryData = paramChangeContract.loadRegistryEntry()
+    //let registryEntryChallengeData = paramChangeContract.loadRegistryEntryChallenge()
+    //let paramChangeData = paramChangeContract.loadParamChange()
+    //let paramChange = Helpers.registryEntry(registryEntryData).merge([
+    //  Helpers.registryEntryChallenge(registryEntryChallengeData),
+    //  Helpers.paramChange(paramChangeData),
+    //])
+    //paramChange.setAddress('regEntry_address', registryEntryAddress)
+    //paramChange.setU256('challenge_createdOn', timestamp)
+    //database.create('ParamChange', registryEntryAddress.toString(), paramChange)
+  } else if (eventType == 'voteCommitted') {
+  } else if (eventType == 'depositTransferred') {
+    return
+  } else if (eventType == 'minted') {
+    return
+  } else if (eventType == 'changeApplied') {
+    return
+  }
+}
