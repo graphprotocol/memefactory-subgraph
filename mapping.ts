25eef4c0016a57084b51232d6b59d4ebf33f6b4e
Brandon Ramirez <brandon.ramirez@live.com>
2018-06-29 00:19:42 -0700

    Add mapping for 'created' and 'challengeCreated' RegistryEntryEvent eventTypes

--- mapping.ts
+++ mapping.ts
@@ -1,7 +1,7 @@
 /// <reference path="./node_modules/assemblyscript/std/assembly.d.ts" />
 /// <reference path="./node_modules/the-graph-wasm/index.d.ts" />
-/// <reference path="./types/Meme.types.ts" />
-/// <reference path="./types/MemeRegistry.types.ts" />
+/// <reference path="./dist/Meme.types.ts" />
+/// <reference path="./dist/MemeRegistry.types.ts" />
 
 export function handleRegistryEntryEvent(event: EthereumEvent): void {
   // Extract event arguments
@@ -14,16 +14,59 @@ export function handleRegistryEntryEvent(event: EthereumEvent): void {
 
     // Obtain registry entry and meme data from the contract
     let registryEntryData = memeContract.loadRegistryEntry()
+    let registryEntryChallengeData = memeContract.loadRegistryEntryChallenge()
     let memeData = memeContract.loadMeme()
 
     // Create an entity to push into the database
     let meme = new Entity()
     meme.setAddress('regEntry_address', registryEntryAddress)
     meme.setU256('regEntry_version', registryEntryData.value0)
+    meme.setU32('regEntry_status', registryEntryData.value1)
+    meme.setAddress('regEntry_creator', registryEntryData.value2)
+    meme.setU256('regEntry_deposit', registryEntryData.value3)
+    meme.setU256('regEntry_challengePeriodEnd', registryEntryData.value4)
+
+    meme.setAddress('challenge_challenger', registryEntryChallengeData.value1)
+    meme.setU256('challenge_rewardPool', registryEntryChallengeData.value2)
+    meme.setBytes('challenge_metaHash', registryEntryChallengeData.value3)
+    meme.setU256('challenge_commitPeriodEnd', registryEntryChallengeData.value4)
+    meme.setU256('challenge_revealPeriodEnd', registryEntryChallengeData.value5)
+    meme.setU256('challenge_votesFor', registryEntryChallengeData.value6)
+    meme.setU256('challenge_votesAgainst', registryEntryChallengeData.value7)
+    meme.setU256('challenge_claimedRewardOn', registryEntryChallengeData.value8)
+
+    meme.setU256('regEntry_createdOn', event.params[3].value.toU256())
 
     database.create('Meme', registryEntryAddress.toString(), meme)
+
   } else if (eventType === 'challengeCreated') {
-    return
+    let memeContract = new Meme(registryEntryAddress, event.blockHash)
+
+    let registryEntryData = memeContract.loadRegistryEntry()
+    let registryEntryChallengeData = memeContract.loadRegistryEntryChallenge()
+    let memeData = memeContract.loadMeme()
+
+    let meme = new Entity()
+    meme.setAddress('regEntry_address', registryEntryAddress)
+    meme.setU256('regEntry_version', registryEntryData.value0)
+    meme.setU32('regEntry_status', registryEntryData.value1)
+    meme.setAddress('regEntry_creator', registryEntryData.value2)
+    meme.setU256('regEntry_deposit', registryEntryData.value3)
+    meme.setU256('regEntry_challengePeriodEnd', registryEntryData.value4)
+
+    meme.setAddress('challenge_challenger', registryEntryChallengeData.value1)
+    meme.setU256('challenge_rewardPool', registryEntryChallengeData.value2)
+    meme.setBytes('challenge_metaHash', registryEntryChallengeData.value3)
+    meme.setU256('challenge_commitPeriodEnd', registryEntryChallengeData.value4)
+    meme.setU256('challenge_revealPeriodEnd', registryEntryChallengeData.value5)
+    meme.setU256('challenge_votesFor', registryEntryChallengeData.value6)
+    meme.setU256('challenge_votesAgainst', registryEntryChallengeData.value7)
+    meme.setU256('challenge_claimedRewardOn', registryEntryChallengeData.value8)
+
+    meme.setU256('challenge_createdOn', event.params[3].value.toU256())
+
+    database.update('Meme', registryEntryAddress.toString(), meme)
+    
   } else if (eventType === 'voteCommitted') {
     return
   } else if (eventType === 'voteRevealed') {
